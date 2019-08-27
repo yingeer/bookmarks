b1a1adefadfbe7c464f37746956ca403eaee1e02
ying <2822339490@qq.com>
2019-08-27 08:27:29 +0800

    重写ModelForm 中的save方法

--- imageapp/forms.py
+++ imageapp/forms.py
@@ -14,4 +14,14 @@ class ImageForm(forms.ModelForm):
             "url": "link",
         }
 
+    def clean_url(self):
+        url = self.cleaned_data['url']
+        valid_extension = ["jpg", "jpeg"]
+        cur_extension = url.split(".")[-1]
+        if cur_extension not in valid_extension:
+            raise forms.ValidationError("The given URL does not ' \
+                                   'match valid image extensions.")
+        
+        return url
+            
         
